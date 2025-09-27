f052557f62ca9554f4656074ac748a36c9ca4033
Adnan Assad <adnnn336@gmail.com>
2025-09-27 07:25:34 +0600

    third commit

--- Flower_Shop/settings.py
+++ Flower_Shop/settings.py
@@ -13,7 +13,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qem4v4irqbl@11535$g(42%opj!1haxehy4o0yhz^^+tk1%5%k'
+# SECRET_KEY = 'django-insecure-qem4v4irqbl@11535$g(42%opj!1haxehy4o0yhz^^+tk1%5%k'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -41,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -72,13 +74,24 @@ WSGI_APPLICATION = 'Flower_Shop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+DEBUG = config('DEBUG', default=True, cast=bool)
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
@@ -104,7 +117,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Asia/Dhaka'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
@@ -120,6 +133,11 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
@@ -131,8 +149,7 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 
 
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+
 LOGIN_URL = 'login'                
 LOGIN_REDIRECT_URL = 'home'       
 LOGOUT_REDIRECT_URL = 'login'
--- shop/urls.py
+++ shop/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/flowers/', views.api_flower_list, name='api_flower_list'),
-    path('api/flowers/<int:flower_id>/', views.api_flower_detail, name='api_flower_detail'),
-    path('api/buy/<int:flower_id>/', views.api_buy_flower, name='api_buy_flower'),
-    path('api/orders/', views.api_order_history, name='api_order'),
+    path('flowers/', views.api_flower_list, name='api_flower_list'),
+    path('flowers/<int:flower_id>/', views.api_flower_detail, name='api_flower_detail'),
+    path('buy/<int:flower_id>/', views.api_buy_flower, name='api_buy_flower'),
+    path('orders/', views.api_order_history, name='api_order'),
 ]
